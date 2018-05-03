93aede75f6b5ded0f0d292c90e8a5aeefced32a6
Gregory Higley <code@revolucent.net>
2018-05-02 22:37:38 -0400

    Initial commit

---
+++ DateMath/DateMath.h
@@ -0,0 +1,19 @@
+//
+//  DateMath.h
+//  DateMath
+//
+//  Created by Gregory Higley on 5/2/18.
+//  Copyright © 2018 Gregory Higley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DateMath.
+FOUNDATION_EXPORT double DateMathVersionNumber;
+
+//! Project version string for DateMath.
+FOUNDATION_EXPORT const unsigned char DateMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateMath/PublicHeader.h>
+
+
