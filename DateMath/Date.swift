bad6a1a85939f50aa6d95a641d3452e5c6e30c8e
Gregory Higley <code@revolucent.net>
2018-05-03 19:30:45 -0400

    Documentation

--- DateMath/Date.swift
+++ DateMath/Date.swift
@@ -8,22 +8,28 @@
 
 import Foundation
 
+/**
+ Recursively and immutably performs date math operations.
+ 
+ This function often rewrites the expression tree as it processes
+ in order to deal with time zones.
+ */
 private func add(to date: Date, expression: Expression) -> (Date, TimeZone) {
     switch expression {
-    case .unit:
+    case .add:
         return add(to: date, expression: .tz(.current) + expression)
-    case let .add(.tz(timeZone), .unit(component, value)):
+    case let .combine(.tz(timeZone), .add(component, value)):
         var calendar = Calendar(identifier: .gregorian)
         calendar.timeZone = timeZone
         return (calendar.date(byAdding: component, value: value, to: date)!, timeZone)
-    case let .add(.tz, .add(.tz(timeZone), e2)):
+    case let .combine(.tz, .combine(.tz(timeZone), e2)):
         return add(to: date, expression: .tz(timeZone) + e2)
-    case let .add(.tz(timeZone), .add(e1, e2)):
+    case let .combine(.tz(timeZone), .combine(e1, e2)):
         let tz = Expression.tz(timeZone)
         return add(to: date, expression: (tz + e1) + (tz + e2))
-    case let .add(.tz, .tz(timeZone)):
+    case let .combine(.tz, .tz(timeZone)):
         return (date, timeZone)
-    case let .add(e1, e2):
+    case let .combine(e1, e2):
         let (date, timeZone) = add(to: date, expression: e1)
         return add(to: date, expression: .tz(timeZone) + e2)
     case let .tz(timeZone):
@@ -31,24 +37,29 @@ private func add(to date: Date, expression: Expression) -> (Date, TimeZone) {
     }
 }
 
+/// Returns the result of adding the `Expression` `rhs` to the `Date` `lhs`.
 public func +(lhs: Date, rhs: Expression) -> Date {
     let (date, _) = add(to: lhs, expression: rhs)
     return date
 }
 
+/// Returns the result of adding the `Expression` `lhs` to the `Date` `rhs`.
 public func +(lhs: Expression, rhs: Date) -> Date {
     return rhs + lhs
 }
 
+/// Sets the `Date` on the left hand side to itself added to the `Expression` on the right-hand side.
 public func +=(lhs: inout Date, rhs: Expression) {
     let (date, _) = add(to: lhs, expression: rhs)
     lhs = date
 }
 
+/// Returns the result of substracting the `Expression` on the right-hand side from the `Date` on the left.
 public func -(lhs: Date, rhs: Expression) -> Date {
     return lhs + -rhs
 }
 
+/// Sets the `Date` on the left hand to itself subtracting the `Expression` on the right hand.
 public func -=(lhs: inout Date, rhs: Expression) {
     lhs = lhs - rhs
 }
--- DateMath/Expression.swift
+++ DateMath/Expression.swift
@@ -16,40 +16,84 @@ precedencegroup TimeZonePrecedence {
 
 infix operator ⁝: TimeZonePrecedence
 
+/**
+ A DateMath expression.
+ 
+ The only supported operation in date math is addition.
+ (Subtraction is just the addition of a negative number.)
+ */
 public indirect enum Expression {
-    case unit(Calendar.Component, Int)
-    case add(Expression, Expression)
+    /// Adds `Int` units of `Calendar.Component`. The units may be negative.
+    case add(Calendar.Component, Int)
+    /// Adds the expression on the right then the one on the left.
+    case combine(Expression, Expression)
+    /// Changes the time zone.
     case tz(TimeZone)
     
+    /// (Recursively) negates the `Expression`.
     var negated: Expression {
         switch self {
-        case let .unit(component, value): return .unit(component, -value)
-        case let .add(e1, e2): return .add(e1.negated, e2.negated)
+        case let .add(component, value): return .add(component, -value)
+        case let .combine(e1, e2): return .combine(e1.negated, e2.negated)
         case .tz: return self
         }
     }
 }
 
+/**
+ Creates an `Expression` specifying the number of units of a
+ calendar component, e.g., `.day * 2` means "2 days".
+ */
 public func *(lhs: Calendar.Component, rhs: Int) -> Expression {
-    return .unit(lhs, rhs)
-}
-
-public func *(lhs: Int, rhs: Calendar.Component) -> Expression {
-    return .unit(rhs, lhs)
-}
-
-public func +(lhs: Expression, rhs: Expression) -> Expression {
     return .add(lhs, rhs)
 }
 
+/**
+ Creates an `Expression` specifying the number of units of a
+ calendar component, e.g., `2 * .day` means "2 days".
+ */
+public func *(lhs: Int, rhs: Calendar.Component) -> Expression {
+    return .add(rhs, lhs)
+}
+
+/**
+ Creates an expression which is the result of combining
+ the two sub-expressions together.
+ */
+public func +(lhs: Expression, rhs: Expression) -> Expression {
+    return .combine(lhs, rhs)
+}
+
+/**
+ Sets the time zone.
+ 
+ Setting the time zone applies to all expressions to the
+ right of this one:
+ 
+ ```
+ .current ⁝ .day * 2
+ ```
+ 
+ This says that `.day * 2` should be applied in the current
+ time zone. The right-most time zone in a sequence of time zones
+ supercedes all others:
+ 
+ ```
+ tz1 ⁝ tz2 ⁝ tz3 ⁝ .day * 2 - .second * 1
+ ```
+ 
+ In the expression above, only `tz3` will have any force.
+ */
 public func ⁝(lhs: TimeZone, rhs: Expression) -> Expression {
     return .tz(lhs) + rhs
 }
 
+/// (Recursively) negates the expression
 public prefix func -(e: Expression) -> Expression {
     return e.negated
 }
 
+/// Subtracts the second expression from the first.
 public func -(lhs: Expression, rhs: Expression) -> Expression {
     return lhs + -rhs
 }
